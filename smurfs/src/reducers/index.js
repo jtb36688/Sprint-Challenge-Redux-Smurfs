4febf0f846550cba0192d22b3f1af9aab6cb3564
Jacob Bryan <jtb36688@gmail.com>
2019-02-01 11:39:40 -0500

    Structured Actions/Reducers
    Began by identifying all desiered store functionality and adding types for action creators for each function, as well as importing those to the reducers and creating switch cases for them

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,46 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  GET_SMURFS_START,
+  GET_SMURFS_SUCCESS,
+  GET_SMURFS_FAILURE,
+  ADD_SMURF_START,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAILURE,
+  UPDATE_SMURF_START,
+  UPDATE_SMURF_SUCCESS,
+  UPDATE_SMURF_FAILURE,
+  DELETE_SMURF_START,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_FAILURE
+} from "../actions/";
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updateId: "",
+  deletingSmurf: false,
+  error: ""
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SMURFS_SUCCESS:
+      return {
+
+      };
+    case ADD_SMURF_SUCCESS:
+      return {
+
+      };
+    case UPDATE_SMURF_SUCCESS:
+      return {
+
+      };
+    case DELETE_SMURF_SUCCESS:
+      return {
+
+      };
+    default:
+      return state;
+  }
+};
